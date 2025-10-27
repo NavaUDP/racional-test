7a3f551379f318432ab8e480bfbae74b4fda45d4
Diego Navarrete <diego.navarrete2@mail.udp.cl>
2025-10-27 20:20:36 -0300

    re-estructuracion

--- api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, Stock, Holding, Transaction
-
-
-admin.site.register(User)
-admin.site.register(Stock)
-admin.site.register(Holding)
-admin.site.register(Transaction)
